f73d5531b9c7b85b92f2ebd3c87b055e7a1a7097
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-27 10:11:27 -0400

    html and css setup

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const ctx = canvas.getContext('2d');
 // set height and width of the play area
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+// creates a new image element
+playerImage.src = 'shadow_dog.png'
+
+// animation loop
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.fillRect(50, 50, 100, 100);
+    requestAnimationFrame(animate)
+};
+
+animate();
