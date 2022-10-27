e3ba6d3f7b7db78bb5e915618bd1c9e8b8e56be0
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-27 10:00:54 -0400

    canvas set up

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas1'); 
+// context is equal to canvas variable
+const ctx = canvas.getContext('2d');
+// console.log(canvas);
+// lets us see all the 2d canvas animatins
+// we now have access to canvas 2d drawings.
+// will be using canvas draw image method mainly
+
+// set height and width of the play area
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
